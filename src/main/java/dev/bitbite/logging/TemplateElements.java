da9c6cee516ada0265576668e8eccbe6b14c775d
Hauke Motzkus <motzkus.hauke@gmail.com>
2022-07-25 14:36:59 +0200

    closes #23

--- src/main/java/dev/bitbite/logging/TemplateElements.java
+++ src/main/java/dev/bitbite/logging/TemplateElements.java
@@ -5,6 +5,8 @@ public class TemplateElements {
 	public static final String DelimiterOpen = "${delimiteropen}";
 	public static final String DelimiterClose = "${delimiterclose}";
 	public static final String DateTime = "${datetime}";
+	public static final String Date = "${date}";
+	public static final String Time = "${time}";
 	public static final String LogLevelFormat = "${loglevelformat}";
 	public static final String LogLevelName = "${loglevelname}";
 	public static final String CategoryFormat = "${categoryformat}";
